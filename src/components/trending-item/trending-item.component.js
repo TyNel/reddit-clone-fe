19f8a4f4ea8df77d7070fbcc9c4907e56bbbed4a
tyler nelson <TyNel94@outlook.com>
2022-03-05 14:24:41 -0800

    Adding dynamic functionality to trending item component

--- src/components/trending-item/trending-item.component.js
+++ src/components/trending-item/trending-item.component.js
@@ -1,27 +1,38 @@
 import "../trending-item/trending-item.styles.css";
 import { Link } from "react-router-dom";
-import { AiOutlineWindows } from "react-icons/ai";
 
 export default function TrendingItem() {
+  const trendingData = [
+    {
+      backgroundImage: "https://bit.ly/3v6Pc9l",
+      title: "test title",
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      logoImg: "https://bit.ly/3vL8kds",
+      subredditName: "r/tester",
+    },
+  ];
+
   return (
     <div className="trending">
-      <div
-        className="trending-container"
-        style={{
-          backgroundImage: `url("https://bit.ly/3v6Pc9l")`,
-        }}
-      >
-        <Link to="/" className="trending-link">
-          <h2 className="trending-title-text">Test Title</h2>
-          <div className="trending-text">
-            lorem oi[[oifjwe[oijffffw[eoijf sdfdsfs
-          </div>
-          <div className="bottom-logo-container">
-            <AiOutlineWindows className="logo" />
-            <span className="trending-logo-link">Test link</span>
-          </div>
-        </Link>
-      </div>
+      {trendingData.map((data) => (
+        <div
+          className="trending-container"
+          style={{
+            backgroundImage: `url${data.backgroundImage}`,
+          }}
+        >
+          <Link to="/" className="trending-link">
+            <h2 className="trending-title-text">{data.title}</h2>
+            <div className="trending-text">{data.info}</div>
+            <div className="bottom-logo-container">
+              <img src={data.logoImg} alt="subreddit logo" className="logo" />
+              <span className="trending-logo-link">
+                {data.subredditName} and more
+              </span>
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
