2a85e5ea045fb8e838dbd282456a86ef6cbd42b9
tyler nelson <TyNel94@outlook.com>
2022-03-12 15:44:11 -0800

    Added header to subreddit component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
+import Subreddit from "./pages/subreddit/subreddit.component";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <HomePage />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/AskReddit" element={<Subreddit />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Trending from "../../components/trending/trending.component";
 import Posts from "../../components/posts/posts.component";
-import Header from "../../components/header/header.component";
 
 export default function HomePage() {
   return (
     <div className="homepage">
-      <Header />
       <div className="container homepage-body">
         <Trending />
         <Posts />
